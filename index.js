113bb108d6410282018909e05a0a2d30da8a6950
adifkhan <pkadif@gmail.com>
2023-01-17 18:28:44 +0600

    user api created

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ async function run() {
     await client.connect();
     const serviceCollection = client.db('doctors_point').collection('services');
     const bookingCollection = client.db('doctors_point').collection('bookings');
+    const userCollection = client.db('doctors_point').collection('users');
 
     // get all services //
     app.get('/service', async (req, res) => {
@@ -32,9 +33,61 @@ async function run() {
     // post booking //
     app.post('/booking', async (req, res) => {
       const booking = req.body;
+      const query = { service: booking.service, patient: booking.patient, date: booking.date };
+      const preBooked = await bookingCollection.findOne(query);
+      if (preBooked) {
+        return res.send({ success: false, booking: preBooked });
+      }
       const result = await bookingCollection.insertOne(booking);
+      return res.send({ success: true, result });
+    });
+
+    /* // test api //
+    app.post('/booked', async (req, res) => {
+      const query = req.body;
+      const cursor = bookingCollection.find(query);
+      const booked = await cursor.toArray();
+      res.send(booked);
+    })
+    // test api // */
+
+    app.get('/availableServices', async (req, res) => {
+      const date = req.query.date;
+      const services = await serviceCollection.find().toArray();
+
+      const query = { date: date };
+      const thatDayBookings = await bookingCollection.find(query).toArray();
+
+      services.forEach(service => {
+        const bookings = thatDayBookings.filter(tdb => tdb.service === service.name);
+        const bookedSlots = bookings.map(tdb => tdb.slot);
+        const availableSlots = service.slots.filter(slot => !bookedSlots.includes(slot));
+        service.slots = availableSlots;
+      })
+      res.send(services);
+    });
+
+    // my appointment //
+    app.get('/booking', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const bookings = await bookingCollection.find(query).toArray();
+      res.send(bookings);
+    });
+
+    // PUT user //
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const user = req.body;
+      const options = { upsert: ture };
+      const updatedDoc = {
+        $set: user
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc, options);
       res.send(result);
     })
+
   }
   finally {
 
